24a4fb9c2b0cff97c2baec412abba39a2d6ba5b5
farazhashmi21 <developerfaraz@yahoo.com>
2022-07-02 05:20:06 +0000

    Grabadge Collector Added to the git command. GIt remote command addition removed;

--- demostore.sh
+++ demostore.sh
@@ -1,8 +1,9 @@
 echo "Project Name: demostore";
+echo $(git gc);
 echo $(git init);
 echo $(git add .);
-echo $(git commit -m "first commit");
+echo $(git commit -m "Grabadge Collector Added to the git command. GIt remote command addition removed;");
 echo $(git branch -M master);
-echo $(git remote add origin https://github.com/farazhashmi21/demostore.git);
+#echo $(git remote add origin https://github.com/farazhashmi21/demostore.git);
 echo $(git remote -v);
 echo $(git push -u origin master);
